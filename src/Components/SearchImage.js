a341905c6d6b82ee334b24ea57400eb9ba19e454
UdayBari3421 <udaybari2003@gmail.com>
2025-02-03 11:09:54 +0530

    Change File

--- src/Components/SearchImage.js
+++ src/Components/SearchImage.js
@@ -11,8 +11,7 @@ const SearchImage = ({ setImages }) => {
       .get("https://api.unsplash.com/search/photos", {
         headers: {
           "Accept-Version": "v1",
-          Authorization:
-            "Client-ID Nj22p9ZYr2XiSKtrIGjH8_m2gyWG3Wr_A2C0nly-E-Y",
+          Authorization: "Client-ID Nj22p9ZYr2XiSKtrIGjH8_m2gyWG3Wr_A2C0nly-E-Y",
         },
         params: {
           query: search,
@@ -29,12 +28,7 @@ const SearchImage = ({ setImages }) => {
   return (
     <div className="search-container">
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={search}
-          placeholder="Search image here..."
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        <input type="text" value={search} placeholder="Search image here..." onChange={(e) => setSearch(e.target.value)} />
         <button type="submit">Seacrh</button>
       </form>
     </div>
